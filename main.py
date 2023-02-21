e5979b919857d6a5208b8376b3b8c47fcd21b1cb
Minna Soudunsaari <117842115+TheMinna@users.noreply.github.com>
2023-02-21 16:06:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from turtle import Turtle, Screen
+
+minna = Turtle()
+screen = Screen()
+
+screen.listen()
+screen.bgpic('bg.jpg')
+
+
+def move_forwards():
+    minna.forward(5)
+
+def turn_right():
+    minna.right(5)
+
+def turn_left():
+    minna.left(5)
+
+def move_backwards():
+    minna.backward(5)
+
+def clear_screen():
+    screen.resetscreen()
+
+def move_circle_right():
+    minna.circle(50, 2)
+
+def move_circle_left():
+    minna.circle(-50, 2)
+
+
+screen.onkey(fun=move_forwards, key='w')
+screen.onkey(fun=move_backwards, key='s')
+
+screen.onkey(fun=turn_left, key='a')
+screen.onkey(fun=turn_right, key='d')
+
+screen.onkey(fun=move_circle_right, key='e')
+screen.onkey(fun=move_circle_left, key='q')
+
+screen.onkey(fun=clear_screen, key='c')
+
+
+
+screen.exitonclick()
